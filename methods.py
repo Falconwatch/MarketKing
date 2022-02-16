304a0af3ff151258b46550d1e7098672ee047975
Sterals <44032757+Sterals@users.noreply.github.com>
2022-02-17 00:34:58 +0300

    Create methods.py

---
+++ methods.py
@@ -0,0 +1,26 @@
+def load_data(self, ticker, period_start=None, period_end=None, step='1d'):
+    '''
+    Returns the historical trade data.
+
+            Parameters:
+                    ticker (str): Stock ticker name
+                    period_start (string): period start date in format "YYYY-MM-DD". If None - first date in source.
+                    period_end (string): period end date in format "YYYY-MM-DD" If None - last date in source.
+                    step (str): target step of values. valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+
+            Returns:
+                    stock_df (Pandas DataFrame): DataFrame with ticker stock data of target period
+    '''
+
+    assert step in ['1m','2m','5m','15m','30m','60m','90m','1h','1d','5d','1wk','1mo','3mo'], "Wrong step value. should be in ['1m','2m','5m','15m','30m','60m','90m','1h','1d','5d','1wk','1mo','3mo']"
+
+    stock_df = yf.download(
+        tickers = ticker,
+        start = period_start,
+        end = period_end,
+        interval = step,
+        group_by = 'ticker',
+        auto_adjust = True,
+        prepost = True,
+    )
+    return stock_df
